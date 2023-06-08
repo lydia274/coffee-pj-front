becdd759600d1d5f992739b8c41482fd273232a9
lydia a <lydie.antonova@gmail.com>
2023-06-08 10:24:18 +0200

    fixed the router bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,33 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import ListsPage from "./pages/Lists";
-import Reviews from "./pages/Reviews";
-import UserProfile from "./components/UserProfile/UserProfile";
-import LogIn from "./components/LogIn/LogIn";
-import SignIn from "./components/SignIn/SignIn";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import AllUsers from "./components/AllUsers/AllUsers";
-import ProtectedRoute from "./pages/ProtectedRoute";
+// import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import ListsPage from "./pages/Lists"
+import Reviews from "./pages/Reviews"
+import UserProfile from "./components/UserProfile/UserProfile"
+import "./App.css"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import AllUsers from "./components/AllUsers/AllUsers"
+import ProtectedRoute from "./pages/ProtectedRoute"
 
 function App() {
   return (
-    <Router>
-    <Header />
-      <Routes>
-    
-        <Route path="/" element={<Homepage />} />
-        <Route path="/lists" element={<ListsPage />} />
-        <Route path="/reviews" element={<Reviews />} />
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route path="/" element={<Footer />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/lists" element={<ListsPage />} />
+          <Route path="/reviews" element={<Reviews />} />
 
-        <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/allusers" element={<AllUsers />} />
+          </Route>
+
           <Route path="/user" element={<UserProfile />} />
         </Route>
-
-        <Route element={<ProtectedRoute />}>
-          <Route path="/allusers" element={<AllUsers />} />
-        </Route>
-
-        <Route path="/login" element={<LogIn />} />
-        <Route
-          path="/signin"
-          element={<SignIn onClose={handleLoginFormClose} />}
-        />
-      </Routes>
-  <Footer/>
-  </Router>
-
-      
-    </>
+      </Route>
+    </Routes>
   )
-
-
-  );
-
 }
 
 export default App
