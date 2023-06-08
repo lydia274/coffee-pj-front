747f57959c0ce095fd80ca492e03f7f28c063069
lydia a <lydie.antonova@gmail.com>
2023-06-08 19:05:49 +0200

    search bar removed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from "react";
-import NavBar from "../NavBar/NavBar";
-import SearchBar from "../SearchBar/SearchBar";
-import SignIn from "../SignIn/SignIn";
-import LogIn from "../LogIn/LogIn";
-import LogOut from "../LogOut/LogOut";
-import { Outlet } from "react-router-dom";
+import React, { useState } from "react"
+import NavBar from "../NavBar/NavBar"
+import SignIn from "../SignIn/SignIn"
+import LogIn from "../LogIn/LogIn"
+import LogOut from "../LogOut/LogOut"
+import { Outlet } from "react-router-dom"
 
 function Header() {
-  const handleSearch = (searchTerm) => {
-    console.log("Performing search for:", searchTerm);
-  };
-
   return (
     <div>
       <header className="header">
         <div className="header-content">
           <h1>Coffee Project</h1>
           <NavBar />
-          <SearchBar onSearch={handleSearch} />
           <div className="user-buttons">
             <LogIn />
             <SignIn />
@@ -27,7 +21,7 @@ function Header() {
       </header>
       <Outlet />
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
