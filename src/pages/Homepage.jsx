f51e82980fdaf00d183e3532b5d7597481fcd871
lydia a <lydie.antonova@gmail.com>
2023-07-26 19:28:57 +0200

    testing guidelines added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,12 +7,14 @@ function Homepage() {
     <>
       <div className="main">
         <div className="test">
-          <h2>Try it out!</h2>
+          <h3>Test it out!</h3>
+          <p>Log in as one of the users below to see each one's features:</p>
           <ul>
-            <li>user@gmail.com</li>
-            <li>editor@gmail.com</li>
-            <li>admin@gmail.com</li>
+            <li>user@gmail.com - display all</li>
+            <li>editor@gmail.com - edit, update and delete items</li>
+            <li>admin@gmail.com - display all users</li>
           </ul>
+          <p>Password: Tabasco123!</p>
         </div>
         <div className="bg-image img-container">
           <div className="hp-text">
